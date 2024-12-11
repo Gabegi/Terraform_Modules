27be57a6ba4e4d24a9a1d9dfbee24b651aa8d4d4
Gabegi <gabriel_pirastru@hotmail.fr>
2024-12-11 15:49:05 +0100

    added mysql module and rg

--- src/provision/main.tf
+++ src/provision/main.tf
@@ -6,5 +6,23 @@ module "naming_convention" {
   environment                = var.environment
 }
 
+module "mysql" {
+  source = "../modules/mysql"
+  rg_name = azurerm_resource_group.rg.name
+  administrator_login = var.administrator_login
+  administrator_pw = var.administrator_pw
+  environment = var.environment
+  location = var.location
+
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = module.naming_convention.for_modules.resource_group
+  location = var.location
+  tags     = local.tags.default
+}
+
+
 
 
--- src/provision/variables.tf
+++ src/provision/variables.tf
@@ -1,19 +1,43 @@
 variable "application_identification" {
   type        = string
   description = "3 char application code"
+  default = "app"
 }
 
 variable "application_subgroup" {
   type        = string
   description = "2 char code according to naming convention"
+  default = "nl"
 }
 
 variable "business_unit" {
     type        = string
     description = "The business unit"
+    default = "important_department"
 }
 
 variable "environment" {
   type        = string
   description = "The environment name"
+  default = "test"
 }
+
+variable "administrator_login" {
+  type        = string
+  description = "The mysql login name"
+  default = "Bob"
+}
+
+variable "administrator_pw" {
+  type        = string
+  description = "The admin mysql password"
+  default = "notsafe"
+}
+
+
+variable "location" {
+  type        = string
+  description = "Where the resources are located"
+  default = "West Europe"
+}
+
